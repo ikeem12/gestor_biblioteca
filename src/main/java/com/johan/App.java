fd2cd716c12c79f6f2475082d47d74fe1163d6fa
Johan Cardenas <baloncestonba222@gmail.com>
2025-08-14 11:12:20 +0200

    chore: inicializar proyecto maven

---
+++ src/main/java/com/johan/App.java
@@ -0,0 +1,13 @@
+package com.johan;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
