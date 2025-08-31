f3e1ec26609ce636f87fb425eba499d4ca6081fb
Johan Cardenas <baloncestonba222@gmail.com>
2025-08-31 11:45:57 +0200

    feat(server): inicalizar el servidor con configuracion basica

--- src/main/java/com/johan/App.java
+++ src/main/java/com/johan/App.java
@@ -1,13 +1,24 @@
 package com.johan;
 
-/**
- * Hello world!
- *
- */
+import io.github.cdimascio.dotenv.Dotenv;
+import org.glassfish.grizzly.http.server.HttpServer;
+import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
+import org.glassfish.jersey.server.ResourceConfig;
+
+import java.net.URI;
+
 public class App 
 {
+    public static HttpServer startServer(){
+        Dotenv dotenv = Dotenv.load();
+        String baseUri = dotenv.get("BASE_URI");
+
+        final ResourceConfig rc = new ResourceConfig().packages("com.johan.controller");
+        return GrizzlyHttpServerFactory.createHttpServer(URI.create(baseUri), rc);
+    }
+
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        startServer();
     }
 }
