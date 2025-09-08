54575626b1530f2b041e6ccdac8da7f2efc40811
Johan Cardenas <baloncestonba222@gmail.com>
2025-09-08 16:14:00 +0200

    feat(app): añadir modulo JavaTimeModule para soporte de fechas en JSON

--- src/main/java/com/johan/App.java
+++ src/main/java/com/johan/App.java
@@ -1,9 +1,16 @@
 package com.johan;
 
+import com.johan.exception.ValidationExceptionHandler;
+
 import io.github.cdimascio.dotenv.Dotenv;
 import org.glassfish.grizzly.http.server.HttpServer;
 import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
+import org.glassfish.jersey.jackson.internal.jackson.jaxrs.json.JacksonJaxbJsonProvider;
 import org.glassfish.jersey.server.ResourceConfig;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 import java.net.URI;
 
@@ -12,8 +19,24 @@ public class App
     public static HttpServer startServer(){
         Dotenv dotenv = Dotenv.load();
         String baseUri = dotenv.get("BASE_URI");
+        
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.registerModule(new JavaTimeModule());
+        mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
 
-        final ResourceConfig rc = new ResourceConfig().packages("com.johan.controller");
+        JacksonJaxbJsonProvider provider = new JacksonJaxbJsonProvider();
+        provider.setMapper(mapper);
+
+        final ResourceConfig rc = new ResourceConfig();
+        
+        rc.packages("com.johan.controller");
+
+        rc.register(org.glassfish.jersey.server.validation.ValidationFeature.class);
+
+        rc.register(provider);
+
+        rc.register(ValidationExceptionHandler.class); 
+
         return GrizzlyHttpServerFactory.createHttpServer(URI.create(baseUri), rc);
     }
 
